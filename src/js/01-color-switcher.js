2d261779c1bf886233ab9b411b3c45b1aa8fb76a
Kateryna Berehovych <kasia-94@ukr.net>
2022-08-20 15:46:06 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+import '../css/common.css';
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const color = {
+  colorId: null,
+  isActive: false,
+};
 
+start.addEventListener('click', onStartClick);
+stop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  if (color.isActive) {
+    return;
+  }
+  color.isActive = true;
+  color.colorId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(color.colorId);
+  color.isActive = false;
+  console.log('Stop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
